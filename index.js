48f2ca19354e6ba44f4f0fc59683a03300e75f2d
Oliver Rikkilä <turso@olen.to>
2018-11-18 19:07:28 +0200

    tehtava 4.18

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const config = require('./utils/config');
 
 app.use(cors());
@@ -28,6 +29,7 @@ mongoose
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 const server = http.createServer(app);
 
